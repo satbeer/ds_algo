1dcab90275f7fab7c7d171478bc3a564ee233a8e
Satbeer <satbeer@satbeer-pc.(none)>
2014-09-16 00:34:05 +0530

    modified sudoku

--- src/com/play/sudoku/SudokuSolver.java
+++ src/com/play/sudoku/SudokuSolver.java
@@ -2,11 +2,33 @@ package com.play.sudoku;
 
 public class SudokuSolver {
 	
-	private final static int N = 4;
-	protected int[][] grid = {{-1,-1,-1,-1},{-1,-1,-1,-1},{-1,-1,-1,-1},{-1,-1,-1,-1}};
+	private final static int N = 9;
+	protected int[][] grid = {{5,8,-1,		2,-1,-1,	1,-1,-1},
+							  {-1,-1,-1,	8,-1,-1,	2,5,4},
+							  {2,-1,-1,		4,-1,3,		6,8,-1},
+							  {7,6,5,	   3,-1,-1,		8,9,-1},
+							  {-1,2,-1,	  -1,6,9,		-1,-1,-1},
+							  {-1,3,9,	  -1,8,5,		4,6,2},
+							  {-1,4,3,	   5,-1,1,		-1,2,-1},
+							  {-1,1,2,	   -1,-1,-1,	7,-1,-1},
+							  {8,-1,7,		-1,-1,-1,	-1,1,-1}};
+			
+							 /*{{3,9,-1,-1,2,7,5,-1,-1},
+							  {4,7,-1,-1,-1,3,-1,-1,-1},
+							  {-1,-1,-1,4,8,-1,-1,3,-1},
+							  {-1,-1,4,-1,1,5,6,7,-1},
+							  {7,1,-1,-1,-1,-1,-1,5,4},
+							  {-1,5,6,9,7,-1,2,-1,-1},
+							  {-1,4,-1,-1,3,2,-1,-1,-1},
+							  {-1,-1,-1,1,-1,-1,-1,9,5},
+							  {-1,-1,3,8,5,-1,-1,2,7}};*/
+			//{{-1,-1,-1,3},{-1,3,1,-1},{-1,4,3,-1},{-1,-1,-1,1}};//tough
+			//{{3,-1,4,-1},{-1,1,-1,2},{-1,4,-1,3},{2,-1,1,-1}}; //easy
 
 	public static void main(String[] args) {
 		SudokuSolver ss = new SudokuSolver();
+		ss.printSudokuBox(ss.grid);
+		ss.solveSudoku();
 		ss.printSudokuBox(ss.grid);
 	}
 	
@@ -18,18 +40,17 @@ public class SudokuSolver {
 		
 		for(int row=0; row<N; row++){
 			for(int col=0; col<N; col++){
-				if(grid[row][col] == -1){
-					
+				if(grid[row][col] == -1){					
 					
 					for(int k=1; k<=N; k++){
 						if(isSafe(k, row, col)){
 							grid[row][col] = k;
+							if(solveSudoku()){
+								return true;
+							}
 						}
-						if(solveSudoku()){
-							return true;
-						}
-						grid[row][col] = -1;
 					}
+					grid[row][col] = -1;
 				}
 			}
 		}
